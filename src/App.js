6c7622cce7f51178b3baa21dd79d67e4c8b73573
SOMarrah <stephenomarrah@gmail.com>
2022-07-27 10:09:02 -0700

    cube setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import './App.css'
 import { Canvas } from '@react-three/fiber'
 
 export default function App() {
   return (
-    <div>App</div>
+    <div className='CanvasContainer'>
+      <Canvas>
+        <ambientLight intensity={.5}/>
+        <mesh>
+          <boxGeometry/>
+          <meshStandardMaterial/>
+        </mesh>
+      </Canvas>
+    </div>
   )
 }
 
